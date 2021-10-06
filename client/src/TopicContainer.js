018919ed8eb3c1e2c045b6b2c5e842f9dd79a93b
perreta <84996440+perreta@users.noreply.github.com>
2021-10-06 17:50:15 -0400

    trying ScrollToTop again

--- client/src/TopicContainer.js
+++ client/src/TopicContainer.js
@@ -14,6 +14,7 @@ function TopicContainer ({ user, urlCategory, functionalCategory, setURLTopic, e
         .then((resp) => resp.json())
         .then((data) => {
             setTopicArray(data.filter(thread => (thread.category.subject.toLowerCase() === urlCategory)))
+            window.scrollTo(0, 0)
         })
     }, [urlCategory]);
 
